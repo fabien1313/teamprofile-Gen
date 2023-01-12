ba471f88dc72a7412ebe00205e6dcf562923307d
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-11 21:14:49 -0600

    template.js complete...I think

--- src/template.js
+++ src/template.js
@@ -1,24 +1,47 @@
 function dreamTeamRoster(managers, engineers, interns)
 {
     return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Generator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <h1>
+            My Team
+        </h1>
+    </header>
+    <!-- header -->
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    `
-}
+    <!-- main section -->
+    <main>
+        <!-- Manager -->
+        <div id="mgrContainer">
+            ${managerHTML(managers)}
+        </div>
+        <!-- Manager -->
+
+        <!-- Engineers -->
+        <div id="engContainer">
+            ${engineerHTML(engineers)}
+        </div>
+        <!-- EnginSchool -->
+
+        <!-- Interns -->
+        <div id="intContainer">
+            ${internHTML(interns)}
+        </div>
+        <!-- Interns -->
+    </main>  
+</body>
+</html>`
+};
 
 function managerHTML(managers) {
     let mgrSection = ''
@@ -63,15 +86,17 @@ function internHTML(interns) {
     }
     function buildInt(int) {
         return `
-        <div class="engineerDiv">
-                <h2>Engineer</h2>
+        <div class="internDiv">
+                <h2>${int.getRole()}</h2>
                 <ul>
-                    <li>Name: <span></span></li>
-                    <li>ID: <span></span></li>
-                    <li>Email: <span></span></li>
-                    <li>GitHub: <span></span></li>
+                    <li>Name: <span>${int.getName()}</span></li>
+                    <li>ID: <span>${int.getId()}</span></li>
+                    <li>Email: <span>${int.getEmail()}</span></li>
+                    <li>School: <span>${int.getSchool()}</span></li>
                 </ul>
         </div>`
     } return intSection;
 };
+
+module.exports = dreamTeamRoster
 
