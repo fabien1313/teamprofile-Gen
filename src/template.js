ae779c100125dcf3ebfd737d350e10f2f62d32a0
fabien moreno <fabienmoreno1331@yahoo.com>
2023-01-11 21:03:29 -0600

    intern function template

--- src/template.js
+++ src/template.js
@@ -25,7 +25,6 @@ function managerHTML(managers) {
     mgrSection = mgrSection + buildMgr(managers[0])
     function buildMgr(mgr) {
         return `
-        <div id="mgrContainer">
             <div class="managerDiv">
                 <h2>${mgr.getRole()}</h2>
                 <ul>
@@ -34,19 +33,17 @@ function managerHTML(managers) {
                     <li>Email: <span>${mgr.getEmail()}</span></li>
                     <li>Office #: <span>${mgr.getOfficeNumber()}</span></li>
                 </ul>
-            </div>
-        </div> `
+            </div>`
     } return mgrSection;
 };
 
 function engineerHTML(engineers) {
     let engSection = ''
     for (index = 0; index < engineers.length; index++) {
-        engSection = engSection + buildEng(engineers[i]);
+        engSection = engSection + buildEng(engineers[index]);
     }
     function buildEng(eng) {
         return`
-        <div id="engContainer">
             <div class="engineerDiv">
                 <h2>${eng.getRole()}</h2>
                 <ul>
@@ -55,8 +52,26 @@ function engineerHTML(engineers) {
                     <li>Email: <span>${eng.getEmail()}</span></li>
                     <li>GitHub: <span>${eng.getGitHub()}</span></li>
                 </ul>
-            </div>
-        `
+            </div> `
     } return engSection;
+};
+
+function internHTML(interns) {
+    let intSection = ''
+    for (index = 0; index < interns.length; index++) {
+        intSection = intSection + buildInt(interns[index]);  
+    }
+    function buildInt(int) {
+        return `
+        <div class="engineerDiv">
+                <h2>Engineer</h2>
+                <ul>
+                    <li>Name: <span></span></li>
+                    <li>ID: <span></span></li>
+                    <li>Email: <span></span></li>
+                    <li>GitHub: <span></span></li>
+                </ul>
+        </div>`
+    } return intSection;
 };
 
